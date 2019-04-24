bb9b342ad5f582a0f90fdc88a041d9d5b2c2fec2
Sharfik69 <cindersstone@gmail.com>
2019-04-24 16:56:44 +0800

    print all dots 2

--- garbage/garbage_map/views.py
+++ garbage/garbage_map/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from garbage_map.models import Dot
 
+
 def index(request):
     context = {'dots': Dot.objects.all()}
+    print(context['dots'])
     
     return render(request, 'map_yandex/index.html', context)
--- garbage/templates/map_yandex/map_dots.js
+++ garbage/templates/map_yandex/map_dots.js
@@ -1,7 +1,15 @@
 ymaps.ready(init);    
 function init(){ 
     var myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 5
-    }); 
+        center: [52.29778, 104.29639],
+        zoom: 3
+    });
+    myMap.geoObjects      
+    {% for dot in dots %}              
+        .add(new ymaps.Placemark([{{ dot.dot_x }}, {{ dot.dot_y }}], {
+            balloonContent: '{{ dot.subject }}',
+            iconCaption: ''
+        }))
+    {% endfor %}
+    ;
 }
