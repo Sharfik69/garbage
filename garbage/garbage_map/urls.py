969c95b71ddac8b87dd9c5aa14ad6ffce2b2f38d
Sharfik69 <cindersstone@gmail.com>
2019-04-24 11:24:32 +0800

    test

---
+++ garbage/garbage_map/urls.py
@@ -0,0 +1,9 @@
+import re
+
+from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
+from . import views
+
+urlpatterns = [
+    url(r'', views.index, name = "index")
+]
