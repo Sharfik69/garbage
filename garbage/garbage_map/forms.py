9ad0a225c9c3cb2ade936e3e6e59d1a0d681b0f3
Sharfik69 <cindersstone@gmail.com>
2019-05-08 02:56:34 +0800

    add point to address

---
+++ garbage/garbage_map/forms.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from django import forms
+from .models import Dot, Category
+
+class DotForm(forms.ModelForm):
+    class Meta:
+        model = Dot
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'ООО Ромашка'})
+        }
